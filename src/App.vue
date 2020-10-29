269d3490f0eb44fa7ee8d2f48bd3229d4833d992
serii <seriiburduja@gmail.com>
2020-10-29 12:53:09 +0200

    slider

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,71 @@
 <template>
-    <v-app>
-        <v-navigation-drawer
-            app
-            temporary
-            v-model="drawer"
+  <v-app>
+    <v-navigation-drawer
+        app
+        temporary
+        v-model="drawer"
+    >
+      <v-list shaped>
+        <v-list-item
+            v-for="link of links"
+            :key="link.title"
+            :to="link.url"
         >
-            <v-list shaped>
-                <v-list-item
-                    v-for="link of links"
-                    :key="link.title"
-                    :to="link.url"
-                >
-                    <v-list-item-icon>
-                        <v-icon>{{ link.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>{{ link.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-app-bar app>
-            <v-toolbar dark color="primary">
-                <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
-                <v-toolbar-title>
-                    <router-link to="/" tag="span" class="pointer">Add application</router-link>
-                </v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-toolbar-items class="hidden-sm-and-down">
-                    <v-btn text
-                           v-for="link of links"
-                           :key="link.title"
-                           :to="link.url"
-                    >
-                        <v-icon left>{{ link.icon }}</v-icon>
-                        {{ link.title }}
-                    </v-btn>
-                </v-toolbar-items>
-            </v-toolbar>
-        </v-app-bar>
-        <v-content>
-            <router-view></router-view>
-        </v-content>
-    </v-app>
+    <v-app-bar app>
+      <v-toolbar dark color="primary">
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" class="pointer">Add application</router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text
+                 v-for="link of links"
+                 :key="link.title"
+                 :to="link.url"
+          >
+            <v-icon left>{{ link.icon }}</v-icon>
+            {{ link.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
 export default {
-    data() {
-        return {
-            drawer: false,
-            links: [
-                {title: 'Login', icon: 'mdi-lock', url: '/login'},
-                {title: 'Registration', icon: 'mdi-face', url: '/registration'},
-                {title: 'Orders', icon: 'mdi-order-numeric-ascending', url: '/orders'},
-                {title: 'New add', icon: 'mdi-note-plus', url: '/new'},
-                {title: 'My adds', icon: 'mdi-clipboard-list', url: '/list'}
-            ]
-        }
-    },
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-order-numeric-ascending', url: '/orders'},
+        {title: 'New add', icon: 'mdi-note-plus', url: '/new'},
+        {title: 'My adds', icon: 'mdi-clipboard-list', url: '/list'}
+      ]
+    }
+  },
 };
 </script>
 
 <style lang="scss">
 .pointer {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,64 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
-            </v-flex>
-        </v-layout>
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+            <v-carousel-item
+                v-for="(item,i) in filteredAds"
+                :key="i"
+                :src="item.imageSrc"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
     </v-container>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12>
+          <h1>Home</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {}
-    },
+  data() {
+    return {
+      ads: [
+        {
+          title: 'First add',
+          description: 'Desc for First add',
+          promo: true,
+          imageSrc: require('@/assets/img/slider-1.jpg'),
+          id: '123'
+        },
+        {
+          title: 'Second add',
+          description: 'Desc for Second add',
+          promo: true,
+          imageSrc: require('@/assets/img/slider-2.jpg'),
+          id: '223'
+        },
+        {
+          title: 'Third add',
+          description: 'Desc for Third add',
+          promo: false,
+          imageSrc: require('@/assets/img/slider-3.jpg'),
+          id: '323'
+        }
+      ]
+    }
+  },
+  computed: {
+    filteredAds() {
+      return this.ads.filter(item => item.promo)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
