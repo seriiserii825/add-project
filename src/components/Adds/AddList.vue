de91c651037d41db79b2d187213071d053b08ddd
serii <seriiburduja@gmail.com>
2020-10-29 17:42:31 +0200

    addList

--- src/components/Adds/AddList.vue
+++ src/components/Adds/AddList.vue
@@ -1,18 +1,51 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Add list</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <h1 class="text--accent-1 mb-10">My ads</h1>
+    <v-card class="v-elevation-10 pa-10 mb-5" max-width="600" v-for="ad in ads" :key="ad.id">
+      <v-row xs12>
+        <v-col>
+          <v-img height="auto">
+            <img :src="ad.imageSrc" alt="" width="300" height="auto">
+          </v-img>
+        </v-col>
+        <v-col>
+          <v-card-text>
+            {{ ad.title }}
+          </v-card-text>
+          <v-card-text>
+            {{ ad.description }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn class="info" :to="'/ad/' + ad.id">Open</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {}
-    },
+  data() {
+    return {
+      ads: [
+        {
+          title: 'Third add',
+          description: 'Desc for Third add',
+          promo: false,
+          imageSrc: require('@/assets/img/slider-3.jpg'),
+          id: '323'
+        },
+        {
+          title: 'Third add',
+          description: 'Desc for Third add',
+          promo: false,
+          imageSrc: require('@/assets/img/slider-3.jpg'),
+          id: '323'
+        }
+      ]
+    }
+  },
 }
 </script>
 
