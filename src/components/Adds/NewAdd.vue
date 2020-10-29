526f842ac74056d83cf2dc3200e86fd98bfdfd8f
serii <seriiburduja@gmail.com>
2020-10-29 15:33:38 +0200

    create add 10

--- src/components/Adds/NewAdd.vue
+++ src/components/Adds/NewAdd.vue
@@ -1,18 +1,83 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>New add</h1>
+  <v-container>
+    <v-layout row class=" elevation-5">
+      <v-flex xs12 class="pa-10">
+        <h1>Create ad</h1>
+        <v-form
+            ref="form"
+            lazy-validation
+            class="mt-16 pa-16"
+        >
+          <v-text-field
+              prepend-icon="mdi-account"
+              v-model="title"
+              label="Add title"
+              type="text"
+              required
+              :rules="[v => !!v || 'Title is required']"
+          ></v-text-field>
+          <v-text-field
+              prepend-icon="mdi-lock"
+              color="blue"
+              :rules="[v => !!v || 'Description is required']"
+              v-model="description"
+              label="Description"
+              type="text"
+              aria-multiline="true"
+              required
+          ></v-text-field>
+        </v-form>
+        <v-flex>
+          <v-layout row class="mb-4">
+            <v-flex>
+              <v-btn class="warning">Upload image</v-btn>
             </v-flex>
-        </v-layout>
-    </v-container>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex>
+              <!--              <img src="@/assets/img/slider-3.jpg" height="150">-->
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex>
+              <v-switch v-model="promo" label="Add to promo"></v-switch>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row justify-end>
+            <v-flex class="d-inline-block" tag="span" shrink>
+              <v-btn class="success" @click="createAd">Create Ad</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {}
-    },
+  data() {
+    return {
+      title: '',
+      description: '',
+      promo: false
+    }
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          destination: this.description,
+          promo: this.promo
+        }
+        console.log(ad);
+      }
+    }
+  }
 }
 </script>
 
