e4dfa4497c284f6e8c88feb7c0882917d53f2bcd
serii <seriiburduja@gmail.com>
2020-10-31 15:43:13 +0200

    finish site

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,53 +1,56 @@
 <template>
-  <v-container>
-    <h1 class="text--secondary mb-10">Orders</h1>
-    <v-layout row>
-      <v-flex xs12 sm6 offest-sm6>
-        <v-list
-        >
-          <v-list-item v-for="order in orders" :key="order.id">
-            <v-list-item-action>
-              <v-checkbox
-                  :input-value="order.done"
-                  @mousedown="markDone(order)"
-                  color="success"
-              ></v-checkbox>
-            </v-list-item-action>
+    <v-container>
+        <h1 class="text--secondary mb-10">Orders</h1>
+        <v-layout row v-if="!loading && orders.length !== 0">
+            <v-flex xs12 sm6 offest-sm6>
+                <v-list
+                >
+                    <v-list-item v-for="order in orders" :key="order.id">
+                        <v-list-item-action>
+                            <v-checkbox
+                                :input-value="order.done"
+                                @mousedown="markDone(order)"
+                                color="success"
+                            ></v-checkbox>
+                        </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ order.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-btn dark color="blue" :to="'/ad/' + order.adId">Open</v-btn>
-          </v-list-item>
-        </v-list>
-      </v-flex>
-    </v-layout>
-  </v-container>
+                        <v-list-item-content>
+                            <v-list-item-title>{{ order.name }}</v-list-item-title>
+                            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-btn dark color="blue" :to="'/ad/' + order.adId">Open</v-btn>
+                    </v-list-item>
+                </v-list>
+            </v-flex>
+        </v-layout>
+        <v-layout v-else-if="orders.length === 0">
+            <h1>You don't have orders</h1>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      orders: [
-        {
-          id: 'fsds',
-          name: 'Vladilen',
-          phone: '8-992-433-23-43',
-          adId: '123',
-          done: false
+    methods: {
+        markDone(order) {
+            this.$store.dispatch('markOrder', order.id)
+                .then(() => {
+                    order.done = true
+                })
+            .catch(() => {})
         }
-      ]
+    },
+    computed: {
+        loading() {
+            return this.$store.getters.loading
+        },
+        orders() {
+            return this.$store.getters.orders
+        }
+    },
+    created() {
+        this.$store.dispatch('fetchOrders')
     }
-  },
-  methods: {
-    markDone(order) {
-      if (order.done === false) {
-        order.done = true
-      }
-    }
-  }
 }
 </script>
 
--- src/store/orders.js
+++ src/store/orders.js
@@ -18,9 +18,18 @@ export default {
     loadOrders(state, payload) {
       state.orders = payload
     }
-    
   },
-  getters: {},
+  getters: {
+    doneOrders(state) {
+      return state.orders.filter(o => o.done)
+    },
+    unDoneOrders(state) {
+      return state.orders.filter(o => !o.done)
+    },
+    orders(state, getters) {
+      return getters.unDoneOrders.concat(getters.doneOrders)
+    }
+  },
   actions: {
     async createOrder({commit}, {name, phone, adId, ownerId}) {
       const order = new Order(name, phone, adId)
@@ -32,6 +41,45 @@ export default {
         commit('setError', error)
         throw error
       }
+    },
+    async fetchOrders({commit, getters}) {
+      commit('setLoading', true)
+      commit('clearError')
+      
+      const resultOrders = []
+      
+      try {
+        const fbVal = await firebase.database().ref(`/users/${getters.user.id}/orders`).once('value')
+        const orders = fbVal.val()
+        
+        Object.keys(orders).forEach(key => {
+          const o = orders[key]
+          resultOrders.push(
+            new Order(o.name, o.phone, o.adId, o.done, key)
+          )
+        })
+        
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+      }
+    },
+    async markOrder({commit, getters}, payload) {
+      try {
+        commit('clearError')
+        await firebase.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+          done: true
+        })
+        commit('setLoading', false)
+      } catch (error) {
+        
+        console.log(error);
+        commit('setError', error)
+        throw error
+      }
+      
     }
   }
 }
